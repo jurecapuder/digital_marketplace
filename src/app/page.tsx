ff3ac218d71f5005c213080d9d995ebe947332ea
jurecapuder <capuder.j@gmail.com>
2023-11-29 12:26:16 +0100

    Add welcome message to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,12 @@ export default function Home() {
           <span className="text-blue-600">digital assets</span>
           .
         </h1>
+
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+          Welcome to DigitalHippo. Every asset on our
+          platform is verified by our team to ensure our
+          highest quality standards. 
+        </p>
       </div>
     </MaxWidthWrapper>
   )
