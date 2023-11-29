07a8aa1b28f0b075a1f05af40ef3fa607a9a0db3
jurecapuder <capuder.j@gmail.com>
2023-11-29 17:01:54 +0100

    Add perks section to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,15 @@ export default function Home() {
       <section className="border-t border-gray-200 bg-gray-50">
         <MaxWidthWrapper className="py-20">
           <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
-
+            {perks.map((perk) => (
+              <div key={perk.name} className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
+                <div className="md:flex-shrink-0 flex justify-center">
+                  <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">
+                    {<perk.Icon className="w-1/3 h-1/3" />}
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </MaxWidthWrapper>
       </section>
