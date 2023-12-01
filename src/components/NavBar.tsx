f7bd760b12e51fcfcdb11c6e8b7232e43e585a35
jurecapuder <capuder.j@gmail.com>
2023-12-01 11:47:31 +0100

    Add NavItems component to NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Icons } from "./Icons";
+import NavItems from "./NavItems";
 
 const NavBar = () => {
   return (
@@ -15,6 +16,10 @@ const NavBar = () => {
                 <Link href="/">
                   <Icons.logo className="h-10 w-10" />
                 </Link>
+              </div>
+
+              <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+                <NavItems />
               </div>
             </div>
           </div>
