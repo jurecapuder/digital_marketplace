46adb358624b5581cdaf9c2370bfb70c80d73591
jurecapuder <capuder.j@gmail.com>
2023-12-01 12:22:43 +0100

    Add dropdown functionality to NavItem component

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { PRODUCT_CATEGORIES } from "@/config";
 import { Button } from "./ui/button";
+import { ChevronDown } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 type Category = typeof PRODUCT_CATEGORIES[number];
 
@@ -19,8 +23,21 @@ const NavItem = ({
   return (
     <div className="flex">
       <div className="relative flex items-center">
-        <Button className="gap-1.5">
+        <Button
+          className="gap-1.5"
+          onClick={handleOpen}
+          variant={isOpen ? "secondary" : "ghost"}
+        >
+          {category.label}
 
+          <ChevronDown
+            className={cn(
+              "h-4 w-4 transition-all text-muted-foreground",
+              {
+              "-rotate-180" : isOpen
+              }
+            )}
+          />
         </Button>
       </div>
     </div>
