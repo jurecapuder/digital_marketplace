2cf637d00cb47985fc8e6fc6763eb2f32e366724
jurecapuder <capuder.j@gmail.com>
2023-12-01 12:15:48 +0100

    Add dynamic navigation items

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,9 +1,31 @@
 "use client";
 
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useState } from "react";
+import NavItem from "./NavItem";
+
 const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<
+    null | number
+  >(null);
+
   return (
     <div className="flex gap-4 h-full">
-      
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        }
+
+        const isOpen = i === activeIndex;
+
+        return (
+          <NavItem />
+        )
+      })}
     </div>
   )
 }
