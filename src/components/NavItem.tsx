7b2e3dfa366041e2b83708f057fb743ba816c929
jurecapuder <capuder.j@gmail.com>
2023-12-01 12:18:08 +0100

    Refactor NavItem component to include category, handleOpen, isOpen, and isAnyOpen props

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,7 +1,28 @@
-const NavItem = () => {
-  return (
-    <div>
+import { PRODUCT_CATEGORIES } from "@/config";
+import { Button } from "./ui/button";
 
+type Category = typeof PRODUCT_CATEGORIES[number];
+
+interface NavItemProps {
+  category: Category
+  handleOpen: () => void
+  isOpen: boolean
+  isAnyOpen: boolean
+}
+
+const NavItem = ({
+  category,
+  handleOpen,
+  isOpen,
+  isAnyOpen
+} : NavItemProps) => {
+  return (
+    <div className="flex">
+      <div className="relative flex items-center">
+        <Button className="gap-1.5">
+
+        </Button>
+      </div>
     </div>
   )
 }
