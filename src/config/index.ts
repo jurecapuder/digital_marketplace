efe44e8032f0e07baaae70f35f7757fb8f1ef100
jurecapuder <capuder.j@gmail.com>
2023-12-01 12:06:49 +0100

    Add featured UI Kits to product categories

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,22 @@ export const PRODUCT_CATEGORIES = [
   {
     label: "UI Kits",
     value: "ui-kits" as const,
+    featured: [
+      {
+        name: "Editor picks",
+        href: "#",
+        imageSrc: "nav/ui-kits/mixed.jpg"
+      },
+      {
+        name: "New arrivals",
+        href: "#",
+        imageSrc: "nav/ui-kits/blue.jpg"
+      },
+      {
+        name: "Bestsellers",
+        href: "#",
+        imageSrc: "nav/ui-kits/purple.jpg"
+      },
+    ]
   },
 ]
