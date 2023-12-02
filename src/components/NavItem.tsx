0192c5492f327ab959877d9ed144ba17fc3a20c0
jurecapuder <capuder.j@gmail.com>
2023-12-02 18:24:42 +0100

    Add "Shop now" text to NavItem component

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -76,6 +76,13 @@ const NavItem = ({
                       <Link href={item.href} className="mt-6 block font-medium text-gray-900">
                         {item.name}
                       </Link>
+
+                      <p
+                        className="mt-1"
+                        aria-hidden="true"
+                      >
+                        Shop now
+                      </p>
                     </div>
                   ))}
                 </div>
