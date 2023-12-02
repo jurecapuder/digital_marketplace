99d817d175ce87fc61b826cc3daf4002e6fb9e74
jurecapuder <capuder.j@gmail.com>
2023-12-02 18:34:30 +0100

    Add useRef and useOnClickOutside hook to NavItems component

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<
@@ -11,8 +12,14 @@ const NavItems = () => {
 
   const isAnyOpen = activeIndex !== null;
 
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => {
+    setActiveIndex(null);
+  });
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
         const handleOpen = () => {
           if (activeIndex === i) {
