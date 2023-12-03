7d10795629cccc9af636c88e9794970eb54f3d93
jurecapuder <capuder.j@gmail.com>
2023-12-03 20:05:45 +0100

    Add Cart component

---
+++ src/components/Cart.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ShoppingCart } from "lucide-react";
+import { Sheet, SheetTrigger } from "./ui/sheet";
+
+const Cart = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <ShoppingCart className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
+      </SheetTrigger>
+    </Sheet>
+  )
+}
+
+export default Cart;
