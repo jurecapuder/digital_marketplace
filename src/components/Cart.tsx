e7efc83e436292c1da2f8e2e91deea00ecadf358
jurecapuder <capuder.j@gmail.com>
2023-12-03 20:12:03 +0100

    Refactor Cart component to include SheetTrigger className

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -6,8 +6,8 @@ import { Sheet, SheetTrigger } from "./ui/sheet";
 const Cart = () => {
   return (
     <Sheet>
-      <SheetTrigger>
-        <ShoppingCart className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
+      <SheetTrigger className="group -m-2 flex items-center p-2">
+        <ShoppingCart aria-hidden="true" className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
       </SheetTrigger>
     </Sheet>
   )
