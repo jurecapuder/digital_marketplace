849f66cf1f2e54f345d1678a5521719ce634ae6a
jurecapuder <capuder.j@gmail.com>
2023-12-03 20:12:58 +0100

    Refactor Cart component to display the number of items in the cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -7,7 +7,14 @@ const Cart = () => {
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
-        <ShoppingCart aria-hidden="true" className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
+        <ShoppingCart
+          aria-hidden="true"
+          className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+        />
+
+        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+          0
+        </span>
       </SheetTrigger>
     </Sheet>
   )
