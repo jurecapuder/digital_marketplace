13a3c308d4a78ff0f37c7df66da00968afe94c53
jurecapuder <capuder.j@gmail.com>
2023-12-06 20:02:02 +0100

    Add payload.config.ts file

---
+++ src/payload.config.ts
@@ -0,0 +1,18 @@
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { slateEditor } from "@payloadcms/richtext-slate";
+import { buildConfig } from "payload/config";
+
+export default buildConfig({
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
+  collections: [],
+  routes: {
+    admin: "/sell",
+  },
+  admin: {
+
+  },
+  editor: slateEditor({}),
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URL!,
+  }),
+})
