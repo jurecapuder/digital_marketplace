43b25f618784b676abdd92de4c0565211edff29f
jurecapuder <capuder.j@gmail.com>
2023-12-06 20:02:20 +0100

    Add nextApp to server initialization

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { getPayloadClient } from "./get-payload";
-import { nextHandler } from "./next-utils";
+import { nextApp, nextHandler } from "./next-utils";
 
 const app = express();
 
@@ -17,6 +17,14 @@ const start = async () => {
   });
 
   app.use((req, res) => nextHandler(req, res));
+
+  nextApp.prepare().then(() => {
+    payload.logger.info(`Next.js started`);
+
+    app.listen(PORT, async () => {
+      payload.logger.info(`Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`);
+    });
+  });
 }
 
 start();
