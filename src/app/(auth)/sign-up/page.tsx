d96dc3f8b6bc6ea306b88ee3b62fe525073684f2
jurecapuder <capuder.j@gmail.com>
2023-12-07 21:02:02 +0100

    Add account creation title

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -7,6 +7,10 @@ const Page = () => {
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col items-center space-y-2 text-center">
             <Icons.logo className="h-20 w-20" />
+
+            <h1 className="text-2xl font-bold">
+              Create an account
+            </h1>
           </div>
         </div>
       </div>
