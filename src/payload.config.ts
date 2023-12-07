9d460b7f846216ad5d4f7e50e677f16c18ef2f73
jurecapuder <capuder.j@gmail.com>
2023-12-07 20:13:34 +0100

    Add bundler and meta options to admin config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,4 +1,5 @@
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import { buildConfig } from "payload/config";
 
@@ -9,7 +10,15 @@ export default buildConfig({
     admin: "/sell",
   },
   admin: {
-
+    bundler: webpackBundler(),
+    meta: {
+      titleSuffix: " - DigitalHippo",
+      favicon: "/favicon.ico",
+      ogImage: "/thumbnail.jpg"
+    }
+  },
+  rateLimit: {
+    max: 2000
   },
   editor: slateEditor({}),
   db: mongooseAdapter({
