83eba9afd59162f2445b5a9daaea22312f9b57d2
jurecapuder <capuder.j@gmail.com>
2023-12-08 23:54:47 +0100

    Add email and password input fields to sign-up page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -45,6 +45,19 @@ const Page = () => {
                     placeholder="you@example.com"
                   /> 
                 </div>
+
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="password">
+                    Password
+                  </Label>
+
+                  <Input
+                    className={cn({
+                      "focus-visible:ring-red-500": true
+                    })}
+                    placeholder="Password"
+                  /> 
+                </div>
               </div>
             </form>
           </div>
