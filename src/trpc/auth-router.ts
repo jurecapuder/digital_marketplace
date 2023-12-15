0e8dccf02b9806546bd452110e1cc1dd94c22561
jurecapuder <capuder.j@gmail.com>
2023-12-15 17:49:48 +0100

    Add payload retrieval in createPayloadUser mutation

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -1,6 +1,12 @@
 import { AuthCredentialsValidator } from "@/lib/validators/account-credantials-validator";
 import { publicProcedure, router } from "./trpc";
+import { getPayloadClient } from "@/get-payload";
 
 export const authRouter = router({
-  createPayloadUser: publicProcedure.input(AuthCredentialsValidator)
+  createPayloadUser: publicProcedure
+    .input(AuthCredentialsValidator)
+    .mutation(async ({ input }) => {
+      const { email, password } = input;
+      const payload = await getPayloadClient();
+    })
 })
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,9 +1,7 @@
 import { publicProcedure, router } from "./trpc";
 
 export const appRouter = router({
-  anyApiRoute: publicProcedure.query(() => {
-    return "Hello";
-  })
+  auth: authRouter
 });
 
 export type AppRouter = typeof appRouter;
