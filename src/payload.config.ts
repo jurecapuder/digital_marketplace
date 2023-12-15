7afce871c0897f8bc6fe5464e46285ff4aaf8135
jurecapuder <capuder.j@gmail.com>
2023-12-15 20:13:19 +0100

    Add Users collection to payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,14 +3,16 @@ import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import { buildConfig } from "payload/config";
 import path from "path";
+import { Users } from "./collections/Users";
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [],
+  collections: [Users],
   routes: {
     admin: "/sell",
   },
   admin: {
+    user: "users",
     bundler: webpackBundler(),
     meta: {
       titleSuffix: " - DigitalHippo",
