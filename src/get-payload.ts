3007ea618fa30cf6a38fab7d48d1fb894b5ab9fe
jurecapuder <capuder.j@gmail.com>
2023-12-18 21:25:42 +0100

    Update fromAddress in getPayloadClient function

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -46,7 +46,7 @@ export const getPayloadClient = async ({
     cached.promise = payload.init({
       email: {
         transport: transporter,
-        fromAddress: "onboarding@resend.com",
+        fromAddress: "onboarding@resend.dev",
         fromName: "DigitalHippo"
       },
       secret: process.env.PAYLOAD_SECRET,
