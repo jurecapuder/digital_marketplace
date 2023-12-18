e1ff89ee95c1490801d28ba79499c9422bc1c096
jurecapuder <capuder.j@gmail.com>
2023-12-18 20:45:28 +0100

    Add email configuration for payload client

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -44,6 +44,11 @@ export const getPayloadClient = async ({
 
   if (!cached.promise) {
     cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        fromAddress: "onboarding@resend.com",
+        fromName: "DigitalHippo"
+      },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
       ...(initOptions || {})
