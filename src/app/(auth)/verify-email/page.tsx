d04e3190b4cee36c04af13855b7620f4a86f4b09
jurecapuder <capuder.j@gmail.com>
2023-12-19 19:28:48 +0100

    Refactor VerifyEmailPage component to accept searchParams as a prop

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,4 +1,8 @@
-const VerifyEmailPage = () => {
+interface PageProps {
+  searchParams: {[key: string]: string | string[] | undefined};
+}
+
+const VerifyEmailPage = ({ searchParams }: PageProps) => {
   return (
     <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
