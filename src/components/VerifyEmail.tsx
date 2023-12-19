bb59766f2cee6e2f13ca8ab7815d124d6c6cb789
jurecapuder <capuder.j@gmail.com>
2023-12-19 19:50:41 +0100

    Add error handling for invalid email verification token

--- src/components/VerifyEmail.tsx
+++ src/components/VerifyEmail.tsx
@@ -1,9 +1,30 @@
 "use client";
 
 import { trpc } from "@/trpc/client";
+import { XCircle } from "lucide-react";
 
-const VerifyEmail = () => {
-  const {} = trpc.auth
+interface VerifyEmailProps {
+  token: string;
+}
+
+const VerifyEmail = ({ token }: VerifyEmailProps) => {
+  const { data, isLoading, isError } = trpc.auth.verifyEmail.useQuery({
+    token
+  });
+
+  if (isError) {
+    return (
+      <div className="flex flex-col items-center gap-2">
+        <XCircle className="h-8 w-8 text-red-600" />
+
+        <h3 className="font-semibold text-xl">There was a problem</h3>
+
+        <p className="text-muted-foreground text-sm">
+          This token is not valid or might be expired. Please try again.
+        </p>
+      </div>
+    )
+  }
 }
 
 export default VerifyEmail;
