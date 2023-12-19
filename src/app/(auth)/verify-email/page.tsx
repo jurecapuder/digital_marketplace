f33a0d41db274bb4056ee7b3e26e9aef7633ffbe
jurecapuder <capuder.j@gmail.com>
2023-12-19 19:28:04 +0100

    Add VerifyEmailPage component

---
+++ src/app/(auth)/verify-email/page.tsx
@@ -0,0 +1,11 @@
+const VerifyEmailPage = () => {
+  return (
+    <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        {}
+      </div>
+    </div>
+  )
+}
+
+export default VerifyEmailPage;
