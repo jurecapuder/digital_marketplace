d224ac5cac07f41efd0c0e647cc8f2b196c9b0b4
jurecapuder <capuder.j@gmail.com>
2023-12-19 19:34:12 +0100

    Add Image component to verify email page

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface PageProps {
   searchParams: {[key: string]: string | string[] | undefined};
 }
@@ -15,7 +17,11 @@ const VerifyEmailPage = ({ searchParams }: PageProps) => {
         ) : (
           <div className="flex h-full flex-col items-center justify-center space-y-1">
             <div className="relative mb-4 h-60 w-60 text-muted-foreground">
-              
+              <Image
+                src="hippo-email-sent.png"
+                fill
+                alt="Hippo email sent image"
+              />
             </div>
           </div>
         )}
