8f1fe1298431f18bf7aebecb690f12d2f763b3f9
jurecapuder <capuder.j@gmail.com>
2023-12-19 19:48:11 +0100

    Change authRouter verifyEmail mutation to query

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -40,7 +40,7 @@ export const authRouter = router({
 
   verifyEmail: publicProcedure
     .input(z.object({ token: z.string() }))
-    .mutation(async ({ input }) => {
+    .query(async ({ input }) => {
       const { token } = input;
 
       const payload = await getPayloadClient();
