be4c8e6afdcef4f9118e2bee3a879111113295ce
jurecapuder <capuder.j@gmail.com>
2023-12-19 19:44:07 +0100

    Add email verification procedure

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -2,6 +2,7 @@ import { AuthCredentialsValidator } from "../lib/validators/account-credantials-
 import { publicProcedure, router } from "./trpc";
 import { getPayloadClient } from "../get-payload";
 import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
 export const authRouter = router({
   createPayloadUser: publicProcedure
@@ -35,5 +36,13 @@ export const authRouter = router({
         success: true,
         sentToEmail: email
       }
+    }),
+
+  verifyEmail: publicProcedure
+    .input(z.object({ token: z.string() }))
+    .mutation(async ({ input }) => {
+      const { token } = input;
+
+      const payload = await getPayloadClient();
     })
 })
