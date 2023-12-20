d237156d3fe3d7d02b71096184032d32ab6aac83
jurecapuder <capuder.j@gmail.com>
2023-12-20 20:38:48 +0100

    Update sign-in button and add separator
    This commit updates the text on the sign-in button from "Sign up" to "Sign in". It also adds a separator between the sign-in form and the "or" section.

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -119,10 +119,22 @@ const Page = () => {
                 </div>
 
                 <Button>
-                  Sign up
+                  Sign in
                 </Button>
               </div>
             </form>
+
+            <div className="relative">
+              <div aria-hidden="true" className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  or
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
