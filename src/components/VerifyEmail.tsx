a95c675ce1f0a696d42cb1bf511f983bed8fbca1
jurecapuder <capuder.j@gmail.com>
2023-12-20 20:17:20 +0100

    Add loading state to VerifyEmail component

--- src/components/VerifyEmail.tsx
+++ src/components/VerifyEmail.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { trpc } from "@/trpc/client";
-import { XCircle } from "lucide-react";
+import { Loader2, XCircle } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
@@ -52,6 +52,20 @@ const VerifyEmail = ({ token }: VerifyEmailProps) => {
         >
           Sign in
         </Link>
+      </div>
+    )
+  }
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="animate-spin h-8 w-8 text-zinc-300" />
+
+        <h3 className="font-semibold text-xl">Verifying ...</h3>
+
+        <p className="text-muted-foreground text-sm">
+          This won&apos;t take long.
+        </p>
       </div>
     )
   }
