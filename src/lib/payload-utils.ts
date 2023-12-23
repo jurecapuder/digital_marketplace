beb0ad74194a6854fe7f5a50440b39ba44b8a97e
jurecapuder <capuder.j@gmail.com>
2023-12-23 23:59:27 +0100

    Refactor getServerSideUser function to accept cookies parameter

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -1,3 +1,8 @@
-export const getServerSideUser = async () => {
-  
+import { NextRequest } from "next/server"
+import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+
+export const getServerSideUser = async (
+  cookies: NextRequest["cookies"] | ReadonlyRequestCookies
+) => {
+  const token = cookies.get("payload-token")
 }
