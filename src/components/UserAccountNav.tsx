bb072e4a603bf2b7af9cf583175e52b1eb033e7d
jurecapuder <capuder.j@gmail.com>
2023-12-24 16:41:55 +0100

    Add dropdown menu to UserAccountNav component

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,7 +1,19 @@
 "use client";
 
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { DropdownMenu } from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+
 const UserAccountNav = () => {
-  return <></>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="overflow-visible">
+        <Button variant="ghost" size="sm" className="relative">
+          My account
+        </Button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
 };
 
 export default UserAccountNav;
