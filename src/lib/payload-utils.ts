3d40f106e42540f0582740f91b03d0933c9d4c57
jurecapuder <capuder.j@gmail.com>
2023-12-24 16:09:16 +0100

    Add authorization header to fetch request

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -6,5 +6,9 @@ export const getServerSideUser = async (
 ) => {
   const token = cookies.get("payload-token")?.value;
 
-  const meRes = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`);
+  const meRes = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`, {
+    headers: {
+      Authorization: `JWT ${token}`,
+    }
+  });
 }
