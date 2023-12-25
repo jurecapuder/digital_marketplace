9095e877728c55f6456bbdfd50dad3aa2380a757
jurecapuder <capuder.j@gmail.com>
2023-12-25 23:14:33 +0100

    Add seller dashboard link to user account navigation

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -2,11 +2,14 @@
 
 import {
   DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@radix-ui/react-dropdown-menu";
 import { DropdownMenu } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
 import { User } from "../payload-types";
+import Link from "next/link";
 
 const UserAccountNav = ({ user }: { user: User }) => {
   return (
@@ -20,9 +23,17 @@ const UserAccountNav = ({ user }: { user: User }) => {
       <DropdownMenuContent className="bg-white w-60" align="end">
         <div className="flex items-center justify-start gap-2 p-2">
           <div className="flex flex-col space-y-0.5 leading-none">
-            <p className="font-medium text-sm text-black">Username</p>
+            <p className="font-medium text-sm text-black">{user.email}</p>
           </div>
         </div>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem asChild>
+          <Link href="/sell">
+            Seller Dashboard
+          </Link>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
