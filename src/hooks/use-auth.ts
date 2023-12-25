8a4764b82f9d3595f67cac3667052ecafdc6f39b
jurecapuder <capuder.j@gmail.com>
2023-12-25 23:35:00 +0100

    Add useRouter hook and implement sign out functionality

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,6 +1,9 @@
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 export const useAuth = () => {
+  const router = useRouter();
+
   const signOut = async () => {
     try {
       const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/logout`, {
@@ -12,6 +15,12 @@ export const useAuth = () => {
       });
 
       if (!res.ok) throw new Error();
+
+      toast.success("Signed out successfully");
+
+      router.push("/sign-in");
+
+      router.refresh();
     } catch (error) {
       toast.error("Couldn't sign out, please try again.")
     }
