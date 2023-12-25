645aa16a2920f4c85d2725604091d1f7b0da161b
jurecapuder <capuder.j@gmail.com>
2023-12-25 23:31:13 +0100

    Add sign out functionality and error handling

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,11 +1,19 @@
+import { toast } from "sonner";
+
 export const useAuth = () => {
   const signOut = async () => {
     try {
       const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/logout`, {
-        
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json"
+        }
       });
+
+      if (!res.ok) throw new Error();
     } catch (error) {
-      
+      toast.error("Couldn't sign out, please try again.")
     }
   }
 }
