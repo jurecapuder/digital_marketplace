7b15d16d8d6136f17eb214191936f16f212f0a78
jurecapuder <capuder.j@gmail.com>
2023-12-25 20:38:06 +0100

    Add dropdown menu content to UserAccountNav component

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import {
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
 import { DropdownMenu } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
 
@@ -12,6 +15,14 @@ const UserAccountNav = () => {
           My account
         </Button>
       </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="bg-white w-60" align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-0.5 leading-none">
+            <p className="font-medium text-sm text-black">Username</p>
+          </div>
+        </div>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
