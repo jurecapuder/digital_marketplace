56493cb2ca2db2db1541c6ee1af98664c4292ede
jurecapuder <capuder.j@gmail.com>
2023-12-26 23:51:32 +0100

    Add category field to Products collection

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,3 +1,4 @@
+import { PRODUCT_CATEGORIES } from "../../config";
 import { CollectionConfig } from "payload/types";
 
 export const Products: CollectionConfig = {
@@ -34,6 +35,13 @@ export const Products: CollectionConfig = {
       min: 0,
       max: 1000,
       type: "number",
+      required: true,
+    },
+    {
+      name: "category",
+      label: "Category",
+      type: "select",
+      options: PRODUCT_CATEGORIES.map(({ label, value }) => ({ label, value })),
       required: true,
     }
   ]
