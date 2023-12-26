dfa90943b3c28d970aad67067eba306995fd1fc8
jurecapuder <capuder.j@gmail.com>
2023-12-26 23:43:45 +0100

    Add "user" field to Products collection

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -7,6 +7,10 @@ export const Products: CollectionConfig = {
   },
   access: {},
   fields: [
-    
+    {
+      name: "user",
+      type: "relationship",
+      relationTo: "users",
+    }
   ]
 };
