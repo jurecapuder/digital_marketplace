532bf9217364f9031049d410984b10210890ce33
jurecapuder <capuder.j@gmail.com>
2023-12-26 23:43:02 +0100

    Add admin configuration to Products collection

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -2,4 +2,11 @@ import { CollectionConfig } from "payload/types";
 
 export const Products: CollectionConfig = {
   slug: "products",
+  admin: {
+    useAsTitle: "name"
+  },
+  access: {},
+  fields: [
+    
+  ]
 };
