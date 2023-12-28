42367bbbe60cbfc5c42deea733e5a49d141c8c49
jurecapuder <capuder.j@gmail.com>
2023-12-28 18:07:59 +0100

    Update Media.ts with new image sizes and positions

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -17,7 +17,19 @@ export const Media: CollectionConfig = {
         name: "thumbnail",
         width: 400,
         height: 300,
-        position: "center"
+        position: "centre"
+      },
+      {
+        name: "card",
+        width: 768,
+        height: 1024,
+        position: "centre"
+      },
+      {
+        name: "tablet",
+        width: 1024,
+        height: undefined,
+        position: "centre"
       },
     ]
   }
