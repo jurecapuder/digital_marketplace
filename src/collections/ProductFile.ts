b9c54303c5440bdd3b48bdc5d4096df8ba9eadff
jurecapuder <capuder.j@gmail.com>
2023-12-30 00:17:46 +0100

    Add Access Control for Product Files

--- src/collections/ProductFile.ts
+++ src/collections/ProductFile.ts
@@ -1,11 +1,31 @@
 import { User } from "../payload-types";
 import { BeforeChangeHook } from "payload/dist/collections/config/types";
-import { CollectionConfig } from "payload/types";
+import { Access, CollectionConfig } from "payload/types";
 
 const addUser: BeforeChangeHook = ({ req, data }) => {
   const user = req.user as User | null;
   
   return { ...data, user: user?.id };
+};
+
+const yourOwnAndPurchased: Access = async ({ req }) => {
+  const user = req.user as User | null;
+
+  if (user?.role === "admin") return true;
+
+  if (!user) return false;
+
+  const { docs: products } = await req.payload.find({
+    collection: "products",
+    depth: 0,
+    where: {
+      user: {
+        equals: user.id
+      }
+    }
+  });
+
+  const ownProductFileIds = products.map((prod) => prod.product_files);
 };
 
 export const ProductFiles: CollectionConfig = {
@@ -15,6 +35,9 @@ export const ProductFiles: CollectionConfig = {
   },
   hooks: {
     beforeChange: [addUser]
+  },
+  access: {
+    read: yourOwnAndPurchased(),
   },
   upload: {
     staticURL: "/product_files",
