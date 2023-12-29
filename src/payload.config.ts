f6f402968f3c03d9fb42dbf5325ad72bf5292da8
jurecapuder <capuder.j@gmail.com>
2023-12-30 00:40:52 +0100

    Add Orders collection to buildConfig

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,11 +3,12 @@ import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import { buildConfig } from "payload/config";
 import path from "path";
-import { Users } from "./collections/Users";
 import dotenv from "dotenv";
+import { Users } from "./collections/Users";
 import { Products } from "./collections/Products/Products";
 import { Media } from "./collections/Media";
 import { ProductFiles } from "./collections/ProductFile";
+import { Orders } from "./collections/Orders";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env")
@@ -15,7 +16,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users, Products, Media, ProductFiles],
+  collections: [Users, Products, Media, ProductFiles, Orders],
   routes: {
     admin: "/sell",
   },
