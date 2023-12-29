73b2eb4d03673033c7a99651fb2a3517f145f94f
jurecapuder <capuder.j@gmail.com>
2023-12-30 00:04:41 +0100

    Add admin and upload configurations to ProductFiles collection

--- src/collections/ProductFile.ts
+++ src/collections/ProductFile.ts
@@ -2,5 +2,24 @@ import { CollectionConfig } from "payload/types";
 
 export const ProductFiles: CollectionConfig = {
   slug: "product_files",
-  
+  admin: {
+    hidden: ({ user }) => user.role !== "admin"
+  },
+  upload: {
+    staticURL: "/product_files",
+    staticDir: "product_files",
+    mimeTypes: ["image/*", "font/*", "application/postscript"]
+  },
+  fields: [
+    {
+      name: "user",
+      type: "relationship",
+      relationTo: "users",
+      admin: {
+        condition: () => false
+      },
+      hasMany: false,
+      required: true
+    }
+  ]
 }
