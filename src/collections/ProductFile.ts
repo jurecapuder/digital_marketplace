f1fd3ac3b0dad2591d48b72b826a441e15a1e5d4
jurecapuder <capuder.j@gmail.com>
2023-12-30 00:06:44 +0100

    Add beforeChange hook to ProductFiles collection

--- src/collections/ProductFile.ts
+++ src/collections/ProductFile.ts
@@ -1,9 +1,20 @@
+import { User } from "../payload-types";
+import { BeforeChangeHook } from "payload/dist/collections/config/types";
 import { CollectionConfig } from "payload/types";
+
+const addUser: BeforeChangeHook = ({ req, data }) => {
+  const user = req.user as User | null;
+  
+  return { ...data, user: user?.id };
+};
 
 export const ProductFiles: CollectionConfig = {
   slug: "product_files",
   admin: {
     hidden: ({ user }) => user.role !== "admin"
+  },
+  hooks: {
+    beforeChange: [addUser]
   },
   upload: {
     staticURL: "/product_files",
