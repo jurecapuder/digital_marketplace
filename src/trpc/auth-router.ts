da70bca4062bdd36d0ec2c967b9505f856b4d6bb
jurecapuder <capuder.j@gmail.com>
2023-12-31 23:23:58 +0100

    Fix typo in import statement

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -1,4 +1,4 @@
-import { AuthCredentialsValidator } from "../lib/validators/account-credantials-validator";
+import { AuthCredentialsValidator } from "../lib/validators/account-credentials-validator";
 import { publicProcedure, router } from "./trpc";
 import { getPayloadClient } from "../get-payload";
 import { TRPCError } from "@trpc/server";
