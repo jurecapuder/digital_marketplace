5d7ea9add4b5143474aa22ace3f1d759d81d4db6
jurecapuder <capuder.j@gmail.com>
2024-01-01 23:48:19 +0100

    Add section and div elements to ProductReel component

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -1,5 +1,13 @@
 const ProductReel = () => {
-
+  return (
+    <section className="py-12">
+      <div className="md:flex md:items-center md:justify-between mb-4">
+        <div className="max-w-2xl px-4 lg:max-w-4xl lg:px-0">
+          
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default ProductReel;
