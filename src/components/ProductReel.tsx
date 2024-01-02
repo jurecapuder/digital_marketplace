6ec64bacc14d3191b58ac65c30ab63501dcc92f0
jurecapuder <capuder.j@gmail.com>
2024-01-02 21:32:38 +0100

    Add empty grid container for product items

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -36,6 +36,14 @@ const ProductReel = (props: ProductReelProps) => {
           null
         )}
       </div>
+
+      <div className="relative">
+        <div className="mt-6 flex items-center w-full">
+          <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
+            
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
