760e5fe16080e4f56c53f8b7b76ac471dae2f9aa
jurecapuder <capuder.j@gmail.com>
2024-01-02 21:53:47 +0100

    Refactor query options parsing in trpc/index.ts

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -17,6 +17,14 @@ export const appRouter = router({
 
     const payload = await getPayloadClient();
 
+    const parsedQueryOpts: Record<string, { equals: string }> = {};
+
+    Object.entries(queryOpts).forEach(([key, value]) => {
+      parsedQueryOpts[key] = {
+        equals: value
+      }
+    });
+
     const { docs } = await payload.find({
       collection: "products",
       where: {
