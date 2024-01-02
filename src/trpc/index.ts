b103d949ecc88aa23a2741dda2a2565993bd7d54
jurecapuder <capuder.j@gmail.com>
2024-01-02 21:46:52 +0100

    Add publicProcedure for getting infinite products

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,8 +1,34 @@
+import { z } from "zod";
 import { authRouter } from "./auth-router";
-import { router } from "./trpc";
+import { publicProcedure, router } from "./trpc";
+import { QueryValidator } from "../lib/validators/query-validator";
+import { getPayloadClient } from "../get-payload";
 
 export const appRouter = router({
-  auth: authRouter
+  auth: authRouter,
+
+  getInfiniteProducts: publicProcedure.input(z.object({
+    limit: z.number().min(1).max(100),
+    cursor: z.number().nullish(),
+    query: QueryValidator
+  })).query(async ({ input }) => {
+    const { query, cursor } = input;
+    const { sort, limit, ...queryOpts } = query;
+
+    const payload = await getPayloadClient();
+
+    const { docs } = await payload.find({
+      collection: "products",
+      where: {
+        approvedForSale: {
+          equals: "approved"
+        }
+      },
+      sort,
+      depth: 1,
+      limit
+    })
+  })
 });
 
 export type AppRouter = typeof appRouter;
