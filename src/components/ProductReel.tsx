d7ff6cf14a831f8ae20fa0f1907e38748e918907
jurecapuder <capuder.j@gmail.com>
2024-01-02 21:30:39 +0100

    Update Link styling in ProductReel component

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -27,7 +27,7 @@ const ProductReel = (props: ProductReelProps) => {
         </div>
 
         {href ? (
-          <Link href={href}>
+          <Link href={href} className="hidden text-sm font-medium text-blue-600 hover:text-blue-500 md:block">
             Shop the collection {" "}
 
             <span aria-hidden="true">&rarr;</span>
