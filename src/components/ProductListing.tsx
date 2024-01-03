bcf4ed7ff59dc1765983d88b208be3744d2e1ee0
jurecapuder <capuder.j@gmail.com>
2024-01-03 20:12:20 +0100

    Refactor ProductListing component

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -1,5 +1,15 @@
-const ProductListing = () => {
+"use client";
 
+import { Product } from "@/payload-types";
+import { useState } from "react";
+
+interface ProductListingProps {
+  product: Product | null
+  index: number
+}
+
+const ProductListing = ({  }: ProductListingProps) => {
+  const [isVisible, setIsVisible] = useState(false);
 };
 
 export default ProductListing;
