04087cefce982db270c4fc6541b5bfe99e5785ac
jurecapuder <capuder.j@gmail.com>
2024-01-03 20:50:07 +0100

    Add Skeleton component to ProductListing

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -2,6 +2,7 @@
 
 import { Product } from "@/payload-types";
 import { useState } from "react";
+import { Skeleton } from "./ui/skeleton";
 
 interface ProductListingProps {
   product: Product | null
@@ -9,7 +10,7 @@ interface ProductListingProps {
 }
 
 const ProductListing = ({ product, index }: ProductListingProps) => {
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState<boolean>(false);
 
   if (!product || !isVisible) {
     return (
@@ -21,9 +22,15 @@ const ProductListing = ({ product, index }: ProductListingProps) => {
 const ProductPlaceholder = () => {
   return (
     <div className="flex flex-col w-full">
-      <div className="relative bg-zinc-100 aspect-square w-full overflow-hidden">
-        
+      <div className="relative bg-zinc-100 aspect-square w-full overflow-hidden rounded-xl">
+        <Skeleton className="h-full w-full" />
       </div>
+
+      <Skeleton className="mt-4 w-2/3 h-4 rounded-lg" />
+
+      <Skeleton className="mt-2 w-16 h-4 rounded-lg" />
+
+      <Skeleton className="mt-2 w-12 h-4 rounded-lg" />
     </div>
   )
 }
