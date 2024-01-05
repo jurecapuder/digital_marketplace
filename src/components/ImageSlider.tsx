3448b940f0eb19f0012861aeed5adaafef038caf
jurecapuder <capuder.j@gmail.com>
2024-01-05 19:00:06 +0100

    Add inactiveStyles to ImageSlider component

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -3,7 +3,9 @@ interface ImageSliderProps {
 };
 
 const ImageSlider = ({ urls }: ImageSliderProps) => {
-  const activeStyles = "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300"
+  const activeStyles = "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
+
+  const inactiveStyles = "hidden text-gray-400";
 
   return (
     <div className="group relative bg-zinc-100 aspect-square overflow-hidden rounded-xl">
