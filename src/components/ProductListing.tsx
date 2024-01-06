d096bcf32d9816fd1818b6cc86be902cbc3df58b
jurecapuder <capuder.j@gmail.com>
2024-01-06 22:03:17 +0100

    Add validUrls to ImageSlider in ProductListing component

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -32,6 +32,8 @@ const ProductListing = ({ product, index }: ProductListingProps) => {
 
   const label = PRODUCT_CATEGORIES.find(({ value }) => value === product.category)?.label;
 
+  const validUrls = product.images.map(({ image }) => typeof image === "string" ? image : image.url).filter(Boolean) as string[];
+
   if (isVisible && product) {
     return (
       <Link
@@ -42,7 +44,7 @@ const ProductListing = ({ product, index }: ProductListingProps) => {
         href={`/product/${product.id}`}
       >
         <div className="flex flex-col w-full">
-          <ImageSlider />
+          <ImageSlider urls={validUrls} />
 
           <h3 className="mt-4 font-medium text-sm text-gray-700">
             {product.name}
