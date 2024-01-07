3edf70687c4e5faeb1c194ba901853c68121f43f
jurecapuder <capuder.j@gmail.com>
2024-01-07 13:45:07 +0100

    Add slideConfig state to ImageSlider component

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -14,6 +14,11 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
   const [swiper, setSwiper] = useState<null | SwiperType>(null);
   const [activeIndex, setActiveIndex] = useState(0);
 
+  const [slideConfig, setSlideConfig] = useState({
+    isBeginning: true,
+    isEnd: activeIndex === (urls.length ?? 0) - 1,
+  });
+
   const activeStyles = "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
 
   const inactiveStyles = "hidden text-gray-400";
