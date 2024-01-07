9f8382600f5d7c71830b6f9f96bbf1a1cd865287
jurecapuder <capuder.j@gmail.com>
2024-01-07 13:47:27 +0100

    Add useEffect hook to update active index in ImageSlider

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import SwiperType from "swiper";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Pagination } from "swiper/modules";
 
 interface ImageSliderProps {
@@ -18,6 +18,16 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
     isBeginning: true,
     isEnd: activeIndex === (urls.length ?? 0) - 1,
   });
+
+  useEffect(() => {
+    swiper?.on("slideChange", ({ activeIndex }) => {
+      setActiveIndex(activeIndex);
+      setSlideConfig({
+        isBeginning: activeIndex === 0,
+        isEnd: activeIndex === (urls.length ?? 0) - 1,
+      })
+    })
+  }, [swiper, urls]);
 
   const activeStyles = "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
 
