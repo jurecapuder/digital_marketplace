caa8def09012a65b0a524c590efd694a412e2849
jurecapuder <capuder.j@gmail.com>
2024-01-09 23:21:10 +0100

    Add breadcrumb separator icon

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -37,6 +37,17 @@ const Page = ({ params }: PageProps) => {
                     >
                       {breadcrumb.name}
                     </Link>
+
+                    {i !== BREADCRUMBS.length - 1 ? (
+                      <svg
+                        viewBox='0 0 20 20'
+                        fill='currentColor'
+                        aria-hidden='true'
+                        className='ml-2 h-5 w-5 flex-shrink-0 text-gray-300'
+                      >
+                        <path d='M5.555 17.776l8-16 .894.448-8 16-.894-.448z' />
+                      </svg>
+                    ) : null}
                   </div>
                 </li>
               ))}
