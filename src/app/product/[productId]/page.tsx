d5cb1a31f25d533b913c844d15b58743abe7723d
jurecapuder <capuder.j@gmail.com>
2024-01-09 23:17:36 +0100

    Add breadcrumbs to product page

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -4,7 +4,20 @@ interface PageProps {
   params: {
     productId: string;
   }
-}
+};
+
+const BREADCRUMBS = [
+  {
+    id: 1,
+    name: "Home",
+    href: "/"
+  },
+  {
+    id: 2,
+    name: "Products",
+    href: "/products"
+  }
+];
 
 const Page = ({ params }: PageProps) => {
   return (
@@ -12,7 +25,11 @@ const Page = ({ params }: PageProps) => {
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           {/* Product details */}
-          
+          <div className="lg:max-w-lg lg:self-end">
+            <ol className="flex items-center space-x-2">
+
+            </ol>
+          </div>
         </div>
       </div>
     </MaxWidthWrapper>
