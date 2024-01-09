3ad215ce0fc4797cfba000d7f5a43808cc26a8bf
jurecapuder <capuder.j@gmail.com>
2024-01-09 23:15:53 +0100

    Add MaxWidthWrapper component to product page

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,3 +1,5 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 interface PageProps {
   params: {
     productId: string;
@@ -6,8 +8,14 @@ interface PageProps {
 
 const Page = ({ params }: PageProps) => {
   return (
-    <>
-    </>
+    <MaxWidthWrapper className="bg-white">
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          {/* Product details */}
+          
+        </div>
+      </div>
+    </MaxWidthWrapper>
   )
 }
 
