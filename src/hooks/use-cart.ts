c3d1f61df9d6a9feb1a97bed59bd306c402a9370
jurecapuder <capuder.j@gmail.com>
2024-01-12 18:35:26 +0100

    Add useCart hook for managing cart state

---
+++ src/hooks/use-cart.ts
@@ -0,0 +1,15 @@
+import { Product } from "@/payload-types";
+import { create } from "zustand";
+
+export type CartItem = {
+  product: Product
+}
+
+type CartState = {
+  items: CartItem
+  addItem: (product: Product) => void
+  removeItem: (productId: Product) => void
+  clearCart: () => void
+}
+
+export const useCart = create<CartState>()
