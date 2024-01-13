a80875c6b29a86fd6ea881e7806ae141dc2b36b1
jurecapuder <capuder.j@gmail.com>
2024-01-13 22:38:26 +0100

    Add scrollable cart items

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -8,6 +8,8 @@ import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import Image from "next/image";
 import { useCart } from "@/hooks/use-cart";
+import { ScrollArea } from "./ui/scroll-area";
+import CartItem from "./CartItem";
 
 const Cart = () => {
   const { items } = useCart();
@@ -41,8 +43,11 @@ const Cart = () => {
         {itemCount > 0 ? (
           <>
             <div className="flex w-full flex-col pr-6">
-              {/* TODO: cart logic */}
-              Cart items
+              <ScrollArea>
+                {items.map(({ product }) => (
+                  <CartItem key={product.id} />
+                ))}
+              </ScrollArea>
             </div>
 
             <div className="space-y-4 pr-6">
