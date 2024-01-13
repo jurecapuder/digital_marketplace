27b6abd4c9f09a377ce086bdc46ca385e5dba8b0
jurecapuder <capuder.j@gmail.com>
2024-01-13 22:56:43 +0100

    Add remove button to CartItem component

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,10 +1,13 @@
 import { PRODUCT_CATEGORIES } from "@/config";
+import { useCart } from "@/hooks/use-cart";
 import { Product } from "@/payload-types";
-import { ImageIcon } from "lucide-react";
+import { ImageIcon, X } from "lucide-react";
 import Image from "next/image";
 
 const CartItem = ({ product }: {product: Product}) => {
   const { image } = product.images[0];
+
+  const { removeItem } = useCart();
 
   const label = PRODUCT_CATEGORIES.find(({ value }) => value === product.category)?.label;
 
@@ -38,6 +41,17 @@ const CartItem = ({ product }: {product: Product}) => {
             <span className="line-clamp-1 text-xs capitalize text-muted-foreground">
               {label}
             </span>
+
+            <div className="mt-4 text-xs text-muted-foreground">
+              <button
+                onClick={() => removeItem(product.id)}
+                className="flex items-center gap-0.5"
+              >
+                <X className="w-3 h-3" />
+
+                Remove
+              </button>
+            </div>
           </div>
         </div>
       </div>
