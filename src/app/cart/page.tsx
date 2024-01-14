00fdeb7aaec301be09b029153b3af160b060b49f
jurecapuder <capuder.j@gmail.com>
2024-01-14 21:40:51 +0100

    Add useCart hook and update cart page layout

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,4 +1,11 @@
+"use client";
+
+import { useCart } from "@/hooks/use-cart";
+import { cn } from "@/lib/utils";
+
 const Page = () => {
+  const { items, removeItem } = useCart();
+
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
@@ -7,7 +14,9 @@ const Page = () => {
         </h1>
 
         <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
-          
+          <div className={cn("lg:col-span-7", {
+            "rounded-lg border-2 border-dashed border-zinc-200 p-12": items.length === 0
+          })}></div>
         </div>
       </div>
     </div>
