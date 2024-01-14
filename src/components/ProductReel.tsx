204f38d5a67210646f57e938fa505ba3fc6fd57d
jurecapuder <capuder.j@gmail.com>
2024-01-14 22:08:59 +0100

    Refactor ProductReel component to handle null products

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -29,7 +29,7 @@ const ProductReel = (props: ProductReelProps) => {
   let map: (Product | null)[] = [];
 
   if (products && products.length) {
-    map = products;
+    map = products as (Product | null)[];
   } else if (isLoading) {
     map = new Array<null>(query.limit ?? FALLBACK_LIMIT).fill(null);
   };
@@ -66,7 +66,7 @@ const ProductReel = (props: ProductReelProps) => {
         <div className="mt-6 flex items-center w-full">
           <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
             {map.map((product, i) => (
-              <ProductListing key={i} product={product} index={i} />
+              <ProductListing key={product?.id} product={product} index={i} />
             ))}
           </div>
         </div>
