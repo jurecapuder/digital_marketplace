2f84792d571169dfdec8d5983b2f8b1cdffe90a0
jurecapuder <capuder.j@gmail.com>
2024-01-14 21:24:37 +0100

    Refactor UserAccountNav component to improve readability

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -26,7 +26,9 @@ const UserAccountNav = ({ user }: { user: User }) => {
       <DropdownMenuContent className="bg-white w-60" align="end">
         <div className="flex items-center justify-start gap-2 p-2">
           <div className="flex flex-col space-y-0.5 leading-none">
-            <p className="font-medium text-sm text-black">{user.email}</p>
+            <p className="font-medium text-sm text-black">
+              {user.email}
+            </p>
           </div>
         </div>
 
