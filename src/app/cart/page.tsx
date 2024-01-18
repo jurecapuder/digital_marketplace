4d1615f84fc43de8dedfa844285c5502f80da152
jurecapuder <capuder.j@gmail.com>
2024-01-18 19:36:19 +0100

    Add order total with loader

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -177,6 +177,14 @@ const Page = () => {
                 <div className="text-base font-medium text-gray-900">
                   Order Total
                 </div>
+
+                <div className="text-base font-medium text-gray-900">
+                {isMounted ? (
+                    formatPrice(cartTotal + fee)
+                  ) : (
+                    <Loader2 className="h-4 w-4 animate-spin text-muted-foreground" />
+                  )}
+                </div>
               </div>
             </div>
           </section>
