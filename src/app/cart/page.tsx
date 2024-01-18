6cc070e7f2cebb9ae2923d987d433b21cb32ec60
jurecapuder <capuder.j@gmail.com>
2024-01-18 19:35:27 +0100

    Add order total to cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -172,6 +172,12 @@ const Page = () => {
                   )}
                 </div>
               </div>
+
+              <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+                <div className="text-base font-medium text-gray-900">
+                  Order Total
+                </div>
+              </div>
             </div>
           </section>
         </div>
