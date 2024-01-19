3c41656bf497ac0f9339c62447baf21f3b0fe3fa
jurecapuder <capuder.j@gmail.com>
2024-01-19 23:51:31 +0100

    Add user context to createSession mutation

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -2,5 +2,9 @@ import { z } from "zod";
 import { privateProcedure, router } from "./trpc";
 
 export const paymentRouter = router({
-  createSession: privateProcedure.input(z.object({ productIds: z.array(z.string())}))
+  createSession: privateProcedure
+    .input(z.object({ productIds: z.array(z.string()) }))
+    .mutation(({ ctx, input }) => {
+      const { user } = ctx;
+    })
 })
