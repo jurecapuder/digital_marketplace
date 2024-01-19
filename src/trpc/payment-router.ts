c0a525380b9c50e6381db20285083a73ee5e8159
jurecapuder <capuder.j@gmail.com>
2024-01-19 23:50:09 +0100

    Add createSession private procedure to paymentRouter

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -1,5 +1,6 @@
-import { router } from "./trpc";
+import { z } from "zod";
+import { privateProcedure, router } from "./trpc";
 
 export const paymentRouter = router({
-  
+  createSession: privateProcedure.input(z.object({ productIds: z.array(z.string())}))
 })
