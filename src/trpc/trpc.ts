64dbc96ac90e8a590be49862afc69ccc2e24fa83
jurecapuder <capuder.j@gmail.com>
2024-01-19 23:46:05 +0100

    Add authentication middleware for user authorization

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,7 +1,17 @@
+import { User } from "@/payload-types";
 import { ExpressContext } from "@/server";
 import { initTRPC } from "@trpc/server";
+import { PayloadRequest } from "payload/types";
 
 const t = initTRPC.context<ExpressContext>().create();
+
+const middleware = t.middleware;
+
+const isAuth = middleware(async ({ ctx, next }) => {
+  const req = ctx.req as PayloadRequest;
+
+  const { user } = req as { user: User | null };
+});
 
 export const router = t.router;
 
