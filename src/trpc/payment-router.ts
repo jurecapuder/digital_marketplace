3e8d037131e9d6bb50ffbe0132826f5ec0aec83a
jurecapuder <capuder.j@gmail.com>
2024-01-21 22:04:44 +0100

    Add line items to Stripe session creation

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -3,6 +3,7 @@ import { privateProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { getPayloadClient } from "../get-payload";
 import { stripe } from "../lib/stripe";
+import type Stripe from "stripe";
 
 export const paymentRouter = router({
   createSession: privateProcedure
@@ -38,6 +39,8 @@ export const paymentRouter = router({
         }
       });
 
+      const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
       try {
         const stripeSession = await stripe.checkout.sessions.create({
           success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
@@ -45,8 +48,10 @@ export const paymentRouter = router({
           payment_method_types: ["card", "paypal"],
           mode: "payment",
           metadata: {
-            
-          }
+            userId: user.id,
+            orderId: order.id
+          },
+          line_items
         })
       } catch (error) {
         
