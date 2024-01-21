79e6078f961613ece540b9ad0ea25045c29d47b9
jurecapuder <capuder.j@gmail.com>
2024-01-21 21:38:30 +0100

    Add getPayloadClient import to paymentRouter

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
 import { privateProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { getPayloadClient } from "../get-payload";
 
 export const paymentRouter = router({
   createSession: privateProcedure
     .input(z.object({ productIds: z.array(z.string()) }))
-    .mutation(({ ctx, input }) => {
+    .mutation(async ({ ctx, input }) => {
       const { user } = ctx;
 
       let { productIds } = input;
@@ -13,5 +14,7 @@ export const paymentRouter = router({
       if (productIds.length === 0) {
         throw new TRPCError({ code: "BAD_REQUEST" });
       }
+
+      const payload = await getPayloadClient();
     })
 })
