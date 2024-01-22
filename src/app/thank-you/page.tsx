04ceb8825642f9c0822f1ee8e7f8e75da1f4a9e2
jurecapuder <capuder.j@gmail.com>
2024-01-22 13:20:29 +0100

    Add image to ThankYouPage

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+
 const ThankYouPage = () => {
   return (
-    <>
-    </>
+    <main className="relative lg:min-h-full">
+      <div className="h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12">
+        <Image
+          fill
+          src="/checkout-thank-you.jpg"
+          className="h-full w-full object-cover object-center"
+          alt="thank you for your order"
+        />
+      </div>
+    </main>
   )
 }
 
