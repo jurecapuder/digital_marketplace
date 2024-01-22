fc49ddb2ce1d33436499ece1cf933734ddc50545
jurecapuder <capuder.j@gmail.com>
2024-01-22 13:25:08 +0100

    Update ThankYouPage component

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -14,8 +14,12 @@ const ThankYouPage = () => {
 
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-32 xl:gap-x-24">
         <div className="lg:col-start-2">
-          <h1 className="text-sm font-medium text-blue-600">
+          <p className="text-sm font-medium text-blue-600">
             Order successful
+          </p>
+
+          <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Thanks for ordering
           </h1>
         </div>
       </div>
