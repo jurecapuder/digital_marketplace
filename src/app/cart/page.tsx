a02be4a800290332b09f16e6cffad3dd8393d7c8
jurecapuder <capuder.j@gmail.com>
2024-01-22 12:59:23 +0100

    Add createCheckoutSession mutation and useRouter hook

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -4,14 +4,24 @@ import { Button } from "@/components/ui/button";
 import { PRODUCT_CATEGORIES } from "@/config";
 import { useCart } from "@/hooks/use-cart";
 import { cn, formatPrice } from "@/lib/utils";
+import { trpc } from "@/trpc/client";
 import { Check, Loader2, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import React from "react";
 
 const Page = () => {
   const { items, removeItem } = useCart();
+
+  const router = useRouter();
+
+  const { mutate: createCheckoutSession, isLoading } = trpc.payment.createSession.useMutation({
+    onSuccess: ({ url }) => {
+      if (url) router.push(url);
+    }
+  });
 
   const [isMounted, setIsMounted] = useState<boolean>(false);
 
