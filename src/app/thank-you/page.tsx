b9f503611ff63666c8e1b0e943737aefb2fd6fa4
jurecapuder <capuder.j@gmail.com>
2024-01-22 13:22:52 +0100

    Add thank you message on successful order

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -3,13 +3,21 @@ import Image from "next/image";
 const ThankYouPage = () => {
   return (
     <main className="relative lg:min-h-full">
-      <div className="h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12">
+      <div className="hidden md:block h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12">
         <Image
           fill
           src="/checkout-thank-you.jpg"
           className="h-full w-full object-cover object-center"
           alt="thank you for your order"
         />
+      </div>
+
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-32 xl:gap-x-24">
+        <div className="lg:col-start-2">
+          <h1 className="text-sm font-medium text-blue-600">
+            Order successful
+          </h1>
+        </div>
       </div>
     </main>
   )
