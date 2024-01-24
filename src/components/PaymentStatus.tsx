f1fbc76007aa96d3d6647e19fd935a069ed86ca7
jurecapuder <capuder.j@gmail.com>
2024-01-24 17:56:19 +0100

    Refactor PaymentStatus component to display order email

--- src/components/PaymentStatus.tsx
+++ src/components/PaymentStatus.tsx
@@ -1,7 +1,24 @@
-const PaymentStatus = () => {
+"use client";
+
+interface PaymentStatusProps {
+  orderEmail: string;
+  orderId: string;
+  isPaid: boolean;
+}
+
+const PaymentStatus = ({ orderEmail, orderId, isPaid}: PaymentStatusProps) => {
   return (
-    <>
-    </>
+    <div className="mt-16 grid grid-cols-2 gap-x-4 text-sm text-gray-600">
+      <div>
+        <p className="font-medium text-gray-900">
+          Shipping To
+        </p>
+
+        <p>
+          {orderEmail}
+        </p>
+      </div>
+    </div>
   )
 }
 
