5f0d2431b832e5bd8dd9185f811db7497cb947f9
jurecapuder <capuder.j@gmail.com>
2024-01-26 22:20:45 +0100

    Refactor ReceiptEmail component to include props

--- src/components/emails/ReceiptEmail.tsx
+++ src/components/emails/ReceiptEmail.tsx
@@ -1,3 +1,12 @@
-export const ReceiptEmail = ({ }) => {
+import { Product } from "../../payload-types";
+
+interface ReceiptEmailProps {
+  email: string,
+  data: Date,
+  oderId: string,
+  products: Product[]
+}
+
+export const ReceiptEmail = ({ email, data, oderId, products }: ReceiptEmailProps) => {
 
 }
