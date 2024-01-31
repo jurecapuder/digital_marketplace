55976e706e164ad82f958a2dd6b039aabbbb6c64
jurecapuder <capuder.j@gmail.com>
2024-01-31 19:04:32 +0100

    Add email template for primary action

--- src/components/emails/PrimaryActionEmail.tsx
+++ src/components/emails/PrimaryActionEmail.tsx
@@ -1,3 +1,7 @@
+import { Body, Button, Container, Head, Hr, Html, Img, Preview, Section, Text } from "@react-email/components";
+
+import * as React from "react";
+
 interface EmailTemplateProps {
   actionLabel: string,
   buttonText: string,
@@ -5,5 +9,50 @@ interface EmailTemplateProps {
 }
 
 export const EmailTemplate = ({ actionLabel, buttonText, href }: EmailTemplateProps) => {
+  <Html>
+    <Head />
 
+    <Preview>
+      The marketplace for high-quality digital goods.
+    </Preview>
+    
+    <Body style={main}>
+      <Container style={container}>
+      
+      <Img
+        src={`${process.env.NEXT_PUBLIC_SERVER_URL}/hippo-newsletter-sign-up.png`}
+        width="150"
+        height="150"
+        alt="DigitalHippo"
+        style={logo}
+      />
+      
+      <Text style={paragraph}>Hi there,</Text>
+      
+      <Text style={paragraph}>
+        Welcome to DigitalHippo, the marketplace for high quality digital
+        goods. Use the button below to {actionLabel}.
+      </Text>
+      
+      <Section style={btnContainer}>
+        <Button style={button} href={href}>
+          {buttonText}
+        </Button>
+      </Section>
+      
+      <Text style={paragraph}>
+        Best,
+        <br />
+        the DigitalHippo team
+      </Text>
+      
+      <Hr style={hr} />
+      
+      <Text style={footer}>
+        If you did not request this email, you can safely ignore it.
+      </Text>
+      
+      </Container>
+    </Body>
+  </Html>
 }
