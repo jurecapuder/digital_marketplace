c34afe0a8f4d5ca19954c73e1442d15b7cb36d3b
jurecapuder <capuder.j@gmail.com>
2024-02-01 23:44:44 +0100

    Add footer component with MaxWidthWrapper

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
+import MaxWidthWrapper from "./MaxWidthWrapper";
+
 const Footer = () => {
   return (
-    <>
-    </>
+    <footer className="bg-white flex-grow-0">
+      <MaxWidthWrapper>
+        <div className="border-t border-gray-200">
+          
+        </div>
+      </MaxWidthWrapper>
+    </footer>
   )
 }
 
