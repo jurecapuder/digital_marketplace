82354113b09d360e84b66d6f59a1717dd11e20e8
jurecapuder <capuder.j@gmail.com>
2024-02-01 23:41:17 +0100

    Add parse function to handle search parameters

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,22 @@
+import { PRODUCT_CATEGORIES } from "@/config";
+
 type Param = string | string[] | undefined;
 
 interface ProductPageProps {
   searchParams: {[key: string]: Param};
 }
 
+const parse = (param: Param) => {
+  return typeof param === "string" ? param : undefined;
+}
+
 const ProductPage = ({ searchParams }: ProductPageProps) => {
+  const sort = parse(searchParams.sort);
+
+  const category = parse(searchParams.category);
+
+  const label = PRODUCT_CATEGORIES.find(({ value }) => value === category)?.label;
+
   return (
     <>
     </>
