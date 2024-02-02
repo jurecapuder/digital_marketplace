72a247f79bf57213103db238f2ffcc37e9535380
jurecapuder <capuder.j@gmail.com>
2024-02-02 23:35:03 +0100

    Add logo to footer if pathname is not in pathsToMinimize

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,21 @@
+import { usePathname } from "next/navigation";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import { Icons } from "./Icons";
 
 const Footer = () => {
+  const pathsToMinimize = ["/verify-email", "/sign-up", "/sign-in"];
+
+  const pathname = usePathname();
+
   return (
     <footer className="bg-white flex-grow-0">
       <MaxWidthWrapper>
         <div className="border-t border-gray-200">
-          
+          {pathsToMinimize.includes(pathname) ? null : (
+            <div className="flex justify-center">
+              <Icons.logo className="h-12 w-auto" />
+            </div>
+          )}
         </div>
       </MaxWidthWrapper>
     </footer>
