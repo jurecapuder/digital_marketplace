3ad5e5a1966dc24d1ae5c1a92b1974d41896f98b
jurecapuder <capuder.j@gmail.com>
2024-02-03 23:39:28 +0100

    Add links to footer for Terms, Privacy Policy, and Cookie Policy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -56,6 +56,22 @@ const Footer = () => {
               &copy; {new Date().getFullYear()} All Rights Reserved
             </p>
           </div>
+
+          <div className="mt-4 flex items-center justify-center md:mt-0">
+            <div className="flex space-x-8">
+              <Link href="#" className="text-sm text-muted-foreground hover:text-gray-600">
+                Terms
+              </Link>
+
+              <Link href="#" className="text-sm text-muted-foreground hover:text-gray-600">
+                Privacy Policy
+              </Link>
+
+              <Link href="#" className="text-sm text-muted-foreground hover:text-gray-600">
+                Cookie Policy
+              </Link>
+            </div>
+          </div>
         </div>
       </MaxWidthWrapper>
     </footer>
