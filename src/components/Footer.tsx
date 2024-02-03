df6650c311c959f61600f6b92a7fe5a2b1c1d0de
jurecapuder <capuder.j@gmail.com>
2024-02-03 23:37:30 +0100

    Add copyright notice to Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -49,6 +49,14 @@ const Footer = () => {
             </div>
           )}
         </div>
+
+        <div className="py-10 md:flex md:items-center md:justify-between">
+          <div className="text-center md:text-left">
+            <p className="text-sm text-muted-foreground">
+              &copy; {new Date().getFullYear()} All Rights Reserved
+            </p>
+          </div>
+        </div>
       </MaxWidthWrapper>
     </footer>
   )
