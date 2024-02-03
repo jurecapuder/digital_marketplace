a3743623827c7395a6ffff6922b1f60e0bdbf0b2
jurecapuder <capuder.j@gmail.com>
2024-02-04 00:05:26 +0100

    Update creator and metadataBase URLs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -55,10 +55,10 @@ export function constructMetadata({
       title,
       description,
       images: [image],
-      creator: '@joshtriedcoding',
+      creator: '@jurecapuder',
     },
     icons,
-    metadataBase: new URL('https://digitalhippo.up.railway.app'),
+    metadataBase: new URL("https://digitalmarketplace-production-d677.up.railway.app/"),
     ...(noIndex && {
       robots: {
         index: false,
